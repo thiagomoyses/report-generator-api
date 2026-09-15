3921dce330670ced12bf8b61da5d53dc6fdc794a
DA SILVA MOYSES Thiago <tmoyses@roset.fr>
2026-09-15 16:01:40 +0200

    first commit

---
+++ src/main/java/com/roset/report_generator_api/ReportGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.roset.report_generator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roset/report_generator_api/ReportGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roset.report_generator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
