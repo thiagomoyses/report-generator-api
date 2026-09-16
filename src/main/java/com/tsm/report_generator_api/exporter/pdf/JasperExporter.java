b84ee81bab8e51d0f3cb237b1e08b417a28d19fe
DA SILVA MOYSES Thiago <tmoyses@roset.fr>
2026-09-16 16:08:31 +0200

    Create JasperExporter.java

---
+++ src/main/java/com/tsm/report_generator_api/exporter/pdf/JasperExporter.java
@@ -0,0 +1,73 @@
+package com.tsm.report_generator_api.exporter.pdf;
+
+import com.tsm.report_generator_api.dto.ReportDataDto;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleWriterExporterOutput;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JasperExporter {
+    private static final String TEMPLATE_PATH = "reports/";
+
+    public byte[] export(String reportName, ReportDataDto data, String format) {
+        try {
+            // Load .jasper compilated from classpath
+            InputStream templateStream = getClass().getClassLoader()
+                    .getResourceAsStream(TEMPLATE_PATH + reportName + ".jasper");
+
+            if (templateStream == null) {
+                throw new IllegalArgumentException("Template not found: " + reportName);
+            }
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(templateStream);
+
+            // Fill report with data (Using JRBeanCollectionDataSource)
+            JRDataSource dataSource = buildDatasource(data);
+
+            Map<String, Object> jasperParams = new HashMap<>();
+
+            if (data.getHeader() != null) {
+                jasperParams.put("header", data.getHeader());
+            }
+
+            if (data.getFooter() != null) {
+                jasperParams.put("footer", data.getFooter());
+            }
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, jasperParams, dataSource);
+
+            return switch (format.toLowerCase()) {
+                case "pdf" -> JasperExportManager.exportReportToPdf(jasperPrint);
+                case "csv" -> exportToCsv(jasperPrint);
+                default -> JasperExportManager.exportReportToPdf(jasperPrint);
+            };
+        } catch (JRException e) {
+            throw new RuntimeException("Error generation report: ", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private JRDataSource buildDatasource(ReportDataDto data) {
+        Collection<Map<String, ?>> rows = (Collection<Map<String, ?>>) (Collection<?>) data.getRows();
+
+        return new JRMapCollectionDataSource(rows);
+    }
+
+    private byte[] exportToCsv(JasperPrint jasperPrint) throws JRException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        JRCsvExporter exporter = new JRCsvExporter();
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleWriterExporterOutput(baos));
+        exporter.exportReport();
+        return baos.toByteArray();
+    }
+}
