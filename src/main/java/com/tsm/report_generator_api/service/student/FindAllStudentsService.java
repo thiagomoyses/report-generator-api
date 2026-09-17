6554c430dc18f2e497d1e8681ebe0f6c2d99eece
DA SILVA MOYSES Thiago <tmoyses@roset.fr>
2026-09-17 16:40:36 +0200

    Create FindAllStudentsService.java

---
+++ src/main/java/com/tsm/report_generator_api/service/student/FindAllStudentsService.java
@@ -0,0 +1,47 @@
+package com.tsm.report_generator_api.service.student;
+
+import com.tsm.report_generator_api.dto.ReportDataDto;
+import com.tsm.report_generator_api.exporter.pdf.JasperExporter;
+import com.tsm.report_generator_api.repository.student.FindAllStudentsRepository;
+import com.tsm.report_generator_api.service.ReportService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class FindAllStudentsService implements ReportService {
+    private final FindAllStudentsRepository findAllStudentsRepository;
+    private final JasperExporter exporterPdf;
+
+    public FindAllStudentsService(
+            FindAllStudentsRepository findAllStudentsRepository,
+            JasperExporter exporterPdf
+    ) {
+        this.findAllStudentsRepository = findAllStudentsRepository;
+        this.exporterPdf = exporterPdf;
+    }
+
+    @Override
+    public byte[] generateReport(Map<String, String> reportParams, String format) {
+
+        // Get rows
+        List<Map<String, Object>> rows = findAllStudentsRepository.find();
+
+        // Get Exec time
+        String execTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+
+        ReportDataDto data = new ReportDataDto();
+        data.setHeader(Map.of(
+                "reportName", "findAllStudents",
+                "execTime", execTime,
+                "title", "Students List"
+        ));
+
+        data.setRows(rows);
+
+        return exporterPdf.export("findAllStudents", null, format);
+    }
+}
