1a403f2e1fbf5db00b572bedf7b468ed76565306
DA SILVA MOYSES Thiago <tmoyses@roset.fr>
2026-09-17 16:40:15 +0200

    Update ReportFacadeImpl.java
    Add nottation Component

--- src/main/java/com/tsm/report_generator_api/facade/ReportFacadeImpl.java
+++ src/main/java/com/tsm/report_generator_api/facade/ReportFacadeImpl.java
@@ -1,16 +1,21 @@
 package com.tsm.report_generator_api.facade;
 
 import com.tsm.report_generator_api.service.ReportService;
+import com.tsm.report_generator_api.service.student.FindAllStudentsService;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Component
 public class ReportFacadeImpl implements ReportFacade{
     private final Map<String, ReportService> serviceMap = new ConcurrentHashMap<>();
 
     public ReportFacadeImpl(
-
-    ) {}
+            FindAllStudentsService findAllStudentsService
+    ) {
+        serviceMap.put("findallstudents", findAllStudentsService);
+    }
 
     @Override
     public byte[] generateReport(String reportName, Map<String, String> reportParams, String format){
